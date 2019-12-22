f9717380590fdbfcd076a9c8480d77653883ad50
unknown <vaynex@protonmail.com>
2019-12-22 00:25:20 +0000

    Added URL loading support into application

--- RepeatIt/app/src/main/java/com/repeatit/application/MainActivity.java
+++ RepeatIt/app/src/main/java/com/repeatit/application/MainActivity.java
@@ -2,8 +2,10 @@ package com.repeatit.application;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -18,9 +20,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button addButton;
+    private Button addButton, editButton, deleteButton;
     private String TAG = "MainActivity: ";
     private ArrayList<String> tasksArrayList = new ArrayList<String>();
+    private ArrayList<String> tasksContentList = new ArrayList<String>();
     private ListView listView;
     private CustomArrayAdapter customArrayAdapter;
 
@@ -32,6 +35,21 @@ public class MainActivity extends AppCompatActivity {
 
         readSavedTasks(tasksArrayList);
 
+        editButton = findViewById(R.id.editButton);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: editButtonOnClick");
+            }
+        });
+        deleteButton = findViewById(R.id.deleteButton);
+        
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: deleteButtonOnClick");
+            }
+        });
         addButton = findViewById(R.id.newButton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +60,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Set an item click listener for ListView
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the selected item text from ListView
+                String selectedItem = (String) parent.getItemAtPosition(position);
+
+                //read file storing each line into a variable
+                String url, description, duration = "";
+
+                loadURL(selectedItem, tasksContentList);
+                //loadDataIntoListItem(selectedItem, tasksContentList);
+
+
+
+
+            }
+        });
     }
 
 
@@ -77,6 +113,63 @@ public class MainActivity extends AppCompatActivity {
         customArrayAdapter = new CustomArrayAdapter(this, tasksArrayList);
         listView = findViewById(R.id.listView);
         listView.setAdapter(customArrayAdapter);
+
+    }
+
+    private void loadDataIntoListItem(String selectedItem, ArrayList<String> arrayList){
+        FileInputStream fis = null;
+        Context context = getApplicationContext();
+        try {
+            fis = context.openFileInput( selectedItem + ".txt");
+            Log.d(TAG, "onItemClick: " + selectedItem + ".txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        StringBuilder sb = new StringBuilder();
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                arrayList.add(line);
+                sb.append(line).append("\n");
+            }
+            fis.close();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+        String result = sb.toString();
+        String url = arrayList.get(1);
+        String duration = arrayList.get(2);
+        String description = arrayList.get(3);
+    }
+
+    private void loadURL(String taskName, ArrayList<String> tasksContent){
+        Context context = getApplicationContext();
+        FileInputStream fis = null;
+        try {
+            Log.d(TAG, "loadURL: looking for file " + taskName + ".txt" );
+            fis = context.openFileInput( taskName);//+".txt");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        StringBuilder sb = new StringBuilder();
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                Log.d(TAG, "readSavedTasks: " + line);
+                tasksContent.add(line);
+                sb.append(line).append("\n");
+            }
+            fis.close();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+        String url = tasksContentList.get(1);
+        Log.d(TAG, "loadURL: the string at index 1 is:" + url);
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
 
     }
 
